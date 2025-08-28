59d9a547299e2aa65b374afbd4b6c7b9a92c21cb
manali suryawanshi <manali.suryawanshi23@gmail.com>
2025-08-28 11:00:26 +0000

    it working here on development environment but once i deploy on vercel s

---
+++ app/next.config.js
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  devIndicators: {
+    buildActivity: false,
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+};
+
+module.exports = nextConfig;
