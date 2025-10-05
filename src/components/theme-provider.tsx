4681dbc834a60a31911c133d4ed65927ab8514c1
manali suryawanshi <manali.suryawanshi23@gmail.com>
2025-10-05 15:46:24 +0000

    ok do one thing explain me the structure and file so that i can fixed it

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
 
-export function AppThemeProvider({ children }: { children: React.ReactNode }) {
+export function AppThemeProvider({ children }: ThemeProviderProps) {
   return (
     <NextThemesProvider
       attribute="class"
